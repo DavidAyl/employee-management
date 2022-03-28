6c6d1a199139242b58ea8d4a17976f0fd98dbfbc
David Aylward <davidaylward@Davids-Mac-mini.lan>
2022-03-28 15:24:07 -0400

    finished up schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,25 @@ CREATE DATABASE exmployee_manager_db;
 
 USE employee_tracker_db;
 
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NULL,
+    salary INT NULL,
+    department_id INT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCE department(id)
+);
+
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL,
     department_name VARCHAR(30) NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(100) NULL,
+    last_name VARCHAR(30) NULL,
+    manager_id INT NULL,
+    role_id INT NULL
 );
